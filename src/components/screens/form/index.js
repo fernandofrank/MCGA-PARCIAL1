f0c2a4ff06e587fe2b769fd995a06559e5afe261
Fernando Frank <fer.frank.46@gmail.com>
2020-10-14 05:37:11 -0300

    MCGA-PARCIAL1: add edit form

--- src/components/screens/form/index.js
+++ src/components/screens/form/index.js
@@ -7,7 +7,7 @@ function Form(props) {
     return (
         <BrowserRouter>
             <Route exact path="/home">
-                <form className="form" autocomplete="off">
+                <form className="form">
                     <h1>User</h1>
                     <div>
                         <Field className="textBox" name="name" component="input" disabled="disable" placeholder={props.name} />
@@ -18,6 +18,25 @@ function Form(props) {
                     <div className="buttonsBox">
                         <Link className="link" to="/editForm">
                             <button className="buttons">Edit</button>
+                        </Link>
+                        <Link className="link" to="/Counter">
+                            <button className="buttons">Counters</button>
+                        </Link>
+                    </div>
+                </form>
+            </Route>
+            <Route exact path="/editForm">
+                <form className="form" autocomplete="off">
+                    <h1>User</h1>
+                    <div>
+                        <Field className="textBox" name="name" component="input" placeholder={props.name} />
+                    </div>
+                    <div>
+                        <Field className="textBox" name="year" component="input" placeholder={props.year} />
+                    </div>
+                    <div className="buttonsBox">
+                        <Link className="link" to="/home">
+                            <button className="buttons" onClick={props.handleInputChange} >Save</button>
                         </Link>
                         <Link className="link" to="/Counter">
                             <button className="buttons">Counters</button>
