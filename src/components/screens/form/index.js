b050f9680e464a6b5bde4aa7c3c9325ca39f43fe
Fernando Frank <fer.frank.46@gmail.com>
2020-10-14 03:02:08 -0300

    MCGA-PARCIAL1: do the form.js and css

---
+++ src/components/screens/form/index.js
@@ -0,0 +1,31 @@
+import './style.css';
+import React from 'react';
+import { Field } from 'react-final-form';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+
+function Form(props) {
+    return (
+        <BrowserRouter>
+            <Route exact path="/home">
+                <form className="form" autocomplete="off">
+                    <h1>User</h1>
+                    <div>
+                        <Field className="textBox" name="name" component="input" disabled="disable" placeholder={props.name} />
+                    </div>
+                    <div>
+                        <Field className="textBox" name="year" component="input" disabled="disable" placeholder={props.year} />
+                    </div>
+                    <div className="buttonsBox">
+                        <Link className="link" to="/editForm">
+                            <button className="buttons">Edit</button>
+                        </Link>
+                        <Link className="link" to="/Counter">
+                            <button className="buttons">Counters</button>
+                        </Link>
+                    </div>
+                </form>
+            </Route>
+        </BrowserRouter>
+    )
+};
+export default Form;
