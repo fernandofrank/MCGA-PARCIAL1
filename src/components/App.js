6b490f8819d41bcf696b9828eb887a8cca9f4982
Fernando Frank <fer.frank.46@gmail.com>
2020-10-14 03:08:25 -0300

    MCGA-PARCIAL1: components files

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Home from '../components/screens/home';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="home">
+        <Home />
+      </div>
+    </div>
+  );
+}
+
+export default App;
